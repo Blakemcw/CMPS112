455bd94c9ea4ba8fecbc01d4d3301f10c4de62a7
Blake McWilliam <blakecmcwilliam@gmail.com>
2018-10-13 12:54:53 -0700

    Finished handling of goto, if, and print statements.

--- assignment1/sbi.scm
+++ assignment1/sbi.scm
@@ -63,103 +63,166 @@
     (lambda (pair)
             (symbol-put! *function-table* (car pair) (cadr pair)))
     `(
-
-        (log10_2 0.301029995663981195213738894724493026768189881)
-        (sqrt_2  1.414213562373095048801688724209698078569671875)
-        (e       2.718281828459045235360287471352662497757247093)
-        (pi      3.141592653589793238462643383279502884197169399)
-        (div     ,(lambda (x y) (floor (/ x y))))
-        (log10   ,(lambda (x) (/ (log x) (log 10.0))))
-        (mod     ,(lambda (x y) (- x (* (div x y) y))))
-        (quot    ,(lambda (x y) (truncate (/ x y))))
-        (rem     ,(lambda (x y) (- x (* (quot x y) y))))
-        (+       ,+)
-        (^       ,expt)
+        (abs     ,abs)
+        (acos    ,acos)
+        (asin    ,asin)
+        (atan    ,atan)
         (ceil    ,ceiling)
+        (cos     ,cos)
         (exp     ,exp)
         (floor   ,floor)
         (log     ,log)
+        (log10   ,(lambda (x) (/ (log x) (log 10.0))))
+        (log2    ,(lambda (x) (/ (log x) (log 2.0))))
+        (round   ,round)
+        (sin     ,sin)
         (sqrt    ,sqrt)
-        (abs     ,abs)
-        (acos    ,acos)
-        (asin    ,atan)
-     ))
-
-(define (insert-into-symbol-table program)
-    (map (lambda (line) (insert line)) program)
+        (tan     ,tan)
+        (trunc   ,truncate)
+        (div     ,(lambda (x y) (floor (/ x y))))
+        (mod     ,(lambda (x y) (- x (* (div x y) y))))
+        (quot    ,(lambda (x y) (truncate (/ x y))))
+        (rem     ,(lambda (x y) (- x (* (quot x y) y))))
+        (+       ,(lambda (x y) (+ x y)))
+        (-       ,(lambda (x y) (- x y)))
+        (^       ,expt)
+     )
 )
 
-(define (insert line)
+(for-each
+    (lambda (pair)
+            (symbol-put! *variable-table* (car pair) (cadr pair)))
+    `(
+        (e       2.718281828459045235360287471352662497757247093)
+        (pi      3.141592653589793238462643383279502884197169399)
+     )
+)
+
+(define (insert-into-symbol-table program)
     (cond 
-        ((null? (cdr line)) null)
-        ((symbol? (cadr line))
-            (symbol-put! *label-table* (cadr line) line)
+        ((null? program) null)
+        ((label? (car program))
+            (symbol-put! *label-table* (cadar program) program)
+            (insert-into-symbol-table (cdr program))
+        )
+        ((not (symbol? (car program)))
+            (printf "~s" (label? (car program)))
+            (insert-into-symbol-table (cdr program))
+        )
+    )
+)
+
+(define (label? line)
+    (cond
+        ((null? line) #f)
+        ((symbol? (car line)) #t)
+        ((not (symbol? (car line))) 
+            (label? (cdr line))
         )
     )
 )
 
 (define (interpret-program program)
+    (printf "~n~s~n" program)
     (cond 
         ((null? program) null)
         ((not (statement? (car program))) (interpret-program (cdr program)))
-        
-        ;; if it is a statement it will be interpreted by another function in place of the printf statement below
-        ((statement? (car program)) 
-            (interpret-statement (car program)) 
-            (interpret-program (cdr program))
+        ((statement? (car program))  
+
+            (let 
+                ([statement-return-value (interpret-statement(extract-statement (car program)))])
+                    ;;(printf "~n--------------~s~n" (null? (cdr program)))
+                    (if (null? statement-return-value)
+                        (interpret-program (cdr program))
+                        (interpret-program (cdr (symbol-get *label-table* statement-return-value)))
+                    )
+            )
         )
+        (else null)
     )
 )
 
 ;; Recursively checks line until it finds a pair aka a statement. Working :)
 (define (statement? line)
-    (cond 
+    (cond
         ((null? (cdr line)) #f)
         ((pair? (cdr line)) #t)
-        ((not (pair? (cdr line))) (statement? (cdr line))))
+        ((not (pair? (cdr line))) (statement? (cdr line)))
+    )
+)
+
+(define (extract-statement line)
+    (cond
+        ((null? (cdr line)) null)
+        ((or (not (pair? (cdr line))) (symbol? (cadr line)))  (extract-statement (cdr line)))
+        ((pair? (cdr line)) (cadr line))
+    )
 )
 
 (define (interpret-statement statement)
-        (cond 
-            ((= "dim"   (car statement))    (interpret-dim statement))
-            ((= "let"   (car statement))    (interpret-let statement))
-            ((= "goto"  (car statement))   (interpret-goto statement))
-            ((= "if"    (car statement))     (interpret-if statement))
-            ((= "print" (car statement))  (interpret-print statement))
-            ((= "input" (car statement))  (interpret-input statement))
-        )
+    (cond
+        ((equal? 'dim    (car statement))    (interpret-dim statement))
+        ((equal? 'let    (car statement))    (interpret-let statement))
+        ((equal? 'goto   (car statement))   (interpret-goto statement))
+        ((equal? 'if     (car statement))     (interpret-if statement))
+        ((equal? 'print  (car statement))  (interpret-print statement))
+        ((equal? 'input  (car statement))  (interpret-input statement))
+    )
 )
 ;; ------------------------------
 ;;     STATEMENT PROCEDURES
 ;; ------------------------------
 
 (define (interpret-dim statement)
-
+        null
 )
 
 (define (interpret-let statement)
-
+        null
 )
 
 (define (interpret-goto statement)
-
+    (cadr statement)
 )
 
 (define (interpret-if statement)
 
+        ;; REPLACE WITH A LET STATEMENT THAT CHECKS CONDITIONS OF EXPRESSIONS!!!
+        (if
+            (((caadr statement) (evaluate-expression (cadadr statement)) (evaluate-expression (cadr (cdadr statement)))))
+            (caddr statement)
+            null
+        )
 )
 
 (define (interpret-print statement)
-
+        (map (lambda (list-of-printables) 
+                (let ([printable list-of-printables])
+                    (cond 
+                        ((string? printable) (printf "~s" (string-trim printable "\"")))
+                        ((expression printable) (print (expression printable)))
+                    )
+                )
+            )
+        (cdr statement))
+    null
 )
 
 (define (interpret-input statement)
-
+        ;(map (cdr statement) (lambda (variable) (symbol-put! *variable-table* variable))
+    null
 )
 
 (define (evaluate-expression expression)
-        
+    (cond 
+        ((number? expression) expression)
+        ((symbol? expression) (hash-ref *variable-table* expression #f))
+        ((pair? expression) (apply (hash-ref *variable-table* (car expression))
+                            (map evaluate-expression (cdr expression))))
+        (else #f)
+    )
 )
+
 
 (define (main arglist)
     (if (or (null? arglist) (not (null? (cdr arglist))))
